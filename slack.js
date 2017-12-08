a9b134132ac1838e81dd6ba4f30f6b7aceef2a69
Javier Delgado <Javier Delgado>
2017-12-07 19:21:12 -0600

    add library to use gpio pins

--- slack.js
+++ slack.js
@@ -4,6 +4,10 @@ var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 var winston = require('winston');
 var env = require('./.env.json');
 
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var LED = new Gpio(2, 'out'); //use GPIO pin 2, and specify that it is output
+var blinkInterval = setInterval(blinkLED, 1000); //run the blinkLED function every 250ms
+
 var bot_token = env.slack.token || '';
 var channel = env.slack.channelId;
 var connected;
@@ -47,3 +51,19 @@ exports.sendMessage = function (message) {
     tryToConnect();
   }
 }
+
+function blinkLED() { //function to start blinking
+  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+    LED.writeSync(1); //set pin state to 1 (turn LED on)
+  } else {
+    LED.writeSync(0); //set pin state to 0 (turn LED off)
+  }
+}
+
+function endBlink() { //function to stop blinking
+  clearInterval(blinkInterval); // Stop blink intervals
+  LED.writeSync(0); // Turn LED off
+  LED.unexport(); // Unexport GPIO to free resources
+}
+
+setTimeout(endBlink, 5000); //stop blinking after 5 seconds
